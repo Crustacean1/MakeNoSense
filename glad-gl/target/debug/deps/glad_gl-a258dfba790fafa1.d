c711e8c44e97dd6d185080b75ec042c22ad43328
Crustacean1 <kamilkowalski314@gmail.com>
2023-04-08 15:57:23 +0200

    feat: add glad

---
+++ glad-gl/target/debug/deps/glad_gl-a258dfba790fafa1.d
@@ -0,0 +1,6 @@
+/home/kamil/Downloads/glad/glad-gl/target/debug/deps/glad_gl-a258dfba790fafa1.rmeta: src/lib.rs src/gl.rs
+
+/home/kamil/Downloads/glad/glad-gl/target/debug/deps/glad_gl-a258dfba790fafa1.d: src/lib.rs src/gl.rs
+
+src/lib.rs:
+src/gl.rs:
